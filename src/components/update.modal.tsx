34ecaf36999599097eafedefe0b04f4accb7328b
hoan.dd <hoan.dd@tinasoft.com.vn>
2024-11-25 14:29:14 +0700

    #28

--- src/components/update.modal.tsx
+++ src/components/update.modal.tsx
@@ -2,20 +2,33 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { mutate } from "swr";
 
 interface IProps {
-  showModalCreate: boolean;
-  setShowModalCreate: (value: boolean) => void;
+  showModalUpdate: boolean;
+  setShowModalUpdate: (value: boolean) => void;
+  blog: IBlog | null;
+  setBlog: (value: IBlog | null) => void;
 }
 
-function CreateModal(props: IProps) {
-  const { showModalCreate, setShowModalCreate } = props;
+function UpdateModal(props: IProps) {
+  const { showModalUpdate, setShowModalUpdate, blog, setBlog } = props;
+  const [id, setId] = useState<number>(0);
   const [title, setTitle] = useState<string>("");
   const [author, setAuthor] = useState<string>("");
   const [content, setContent] = useState<string>("");
+
+  useEffect(() => {
+    if (blog && blog.id) {
+      setId(blog.id);
+      setTitle(blog.title);
+      setAuthor(blog.author);
+      setContent(blog.content);
+    }
+  }, [blog]);
+  
   const handleSubmit = () => {
     if (!title) {
       toast.error("Not empty title !");
@@ -29,8 +42,8 @@ function CreateModal(props: IProps) {
       toast.error("Not empty content !");
       return;
     }
-    fetch("http://localhost:8000/blogs", {
-      method: "POST",
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: "PUT",
       headers: {
         Accept: "application/json, text/plain, */*",
         "Content-Type": "application/json",
@@ -40,7 +53,7 @@ function CreateModal(props: IProps) {
       .then((res) => res.json())
       .then((res) => {
         if (res) {
-          toast.success("Create new blog succeed !");
+          toast.warning("Update blog succeed !");
           handleCloseModal();
           mutate("http://localhost:8000/blogs");
         }
@@ -50,22 +63,20 @@ function CreateModal(props: IProps) {
     setTitle("");
     setAuthor("");
     setContent("");
-    setShowModalCreate(false);
+    setBlog(null);
+    setShowModalUpdate(false);
   };
-  const handleUpdateModal = () => {
-    setShowModalCreate(false);
-  }
   return (
     <>
       <Modal
-        show={showModalCreate}
+        show={showModalUpdate}
         onHide={() => handleCloseModal()}
         backdrop="static"
         keyboard={false}
         size="lg"
       >
         <Modal.Header closeButton>
-          <Modal.Title>Add New A Blog</Modal.Title>
+          <Modal.Title>Update A Blog</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
@@ -102,12 +113,12 @@ function CreateModal(props: IProps) {
           <Button variant="secondary" onClick={() => handleCloseModal()}>
             Close
           </Button>
-          <Button variant="primary" onClick={() => handleSubmit()}>
-            Save
+          <Button variant="warning" onClick={() => handleSubmit()}>
+            Update
           </Button>
         </Modal.Footer>
       </Modal>
     </>
   );
 }
-export default CreateModal;
+export default UpdateModal;
